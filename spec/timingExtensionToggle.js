4375b01423c89f3b3fc34aa820948c345f6e007d
Nigel Charman <nigel.charman.nz@gmail.com>
2016-10-15 14:13:53 +1300

    Publish of Github pages from Gradle.

--- spec/timingExtensionToggle.js
+++ spec/timingExtensionToggle.js
@@ -1,18 +1,40 @@
 window.onload = function() {
-    var toggleButton = document.getElementById("toggle-button");
-    toggleButton.onclick = function() {
 
-        var timings = document.getElementsByClassName("time-fig");
+    var toggleButton = document.getElementById("toggleImg");
+    toggleButton.onclick = handleToggle;
 
-        for (var i = 0; i < timings.length; i++) {
-            var timing = timings[i];
-            console.log(timing);
-
-            if (timing.style.display === "none") {
-                timing.style.display = "block";
-            } else {
-                timing.style.display = "none";
-            }
-         }
-    };
+    toggleTiming(toggleButton.classList.contains("time-toggle-button-on"));
 };
+
+function handleToggle() {
+    var display = false;
+    var toggleButton = document.getElementById("toggleImg");
+
+    // check to see if the button has the class with the on styling
+    if (toggleButton.classList.contains("time-toggle-button-on")) {
+        // show it as off
+        toggleButton.classList.remove("time-toggle-button-on");
+    } else {
+        // show it as on
+        toggleButton.classList.add("time-toggle-button-on");
+        display = true;
+    }
+
+    // update the time figures to reflect the state of the button
+    toggleTiming(display);
+}
+
+function toggleTiming(show) {
+
+    var timings = document.getElementsByClassName("time-fig");
+    var timing;
+    for (var i = 0; timing = timings[i]; i++) {
+        timing.style.display = show ? "inherit" : "none";
+    }
+
+    timings = document.getElementsByClassName("time-fig-inline");
+
+    for (var i = 0; timing = timings[i]; i++) {
+        timing.style.display = show ? "inline" : "none";
+    }
+}
